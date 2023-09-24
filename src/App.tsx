bb49de38afbf0d45302d809df4f6f9696c245cef
eminw <emin@sonra.com>
2023-09-24 14:38:56 +0100

    infra and redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App p-10 max-md:p-4">
-      <header className="App-header">
-        {/* Add your header content here */}
-      </header>
+    <Router>
+      <div className="App p-10 max-md:p-4">
+        <header className="App-header">
+          {/* Add your header content here */}
+        </header>
 
-      {/* Create a navigation menu or header as needed */}
-      
-      <main className="App-main">
-      </main>
-      
-      {/* Add a footer or other content as needed */}
-    </div>
+        {/* Create a navigation menu or header as needed */}
+        
+        <main className="App-main">
+          <Routes>
+          </Routes>
+        </main>
+        
+        {/* Add a footer or other content as needed */}
+      </div>
+    </Router>
   );
 }
 
